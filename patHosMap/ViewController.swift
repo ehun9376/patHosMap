be1343696245d43df901b069572f3920ccdea553
anna328537 <anna328537@gmail.com>
2020-07-16 16:39:40 +0800

    anna

--- patHosMap/ViewController.swift
+++ patHosMap/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Rita改過")
+        print("anna")
         // Do any additional setup after loading the view.
     }
 
