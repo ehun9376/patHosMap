c7c77cfa8f620fb3e95cd8f522effad21d3c3f9d
anna328537 <anna328537@gmail.com>
2020-07-16 23:48:54 +0800

    change storyboard

---
+++ patHosMap/animal_hospital_list.swift
@@ -0,0 +1,30 @@
+//
+//  animal_hospital_list.swift
+//  patHosMap
+//
+//  Created by 123 on 2020/7/16.
+//  Copyright © 2020 陳逸煌. All rights reserved.
+//
+
+import UIKit
+
+class animal_hospital_list: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
