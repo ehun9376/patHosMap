3d9ef683bc9bcdccfbf56a707ad8b0c1815b706f
rita <1992ritahuang@gmail.com>
2020-07-21 11:04:51 +0800

    add Map function

--- patHosMap/HosDetailViewController.swift
+++ patHosMap/HosDetailViewController.swift
@@ -1,18 +1,17 @@
-//
-//  HosDetailViewController.swift
-//  patHosMap
-//
-//  Created by 陳逸煌 on 2020/7/20.
-//  Copyright © 2020 陳逸煌. All rights reserved.
-//
-
 import UIKit
+import MapKit
+import CoreLocation
 
 class HosDetailViewController: UIViewController {
     var strtel = ""
     var straddr = ""
+    var latitude:CLLocationDegrees!
+    var longitude:CLLocationDegrees!
+    let annomation = MKPointAnnotation()
+    @IBOutlet weak var hosName: UILabel!
     @IBOutlet weak var hosTelephone: UILabel!
     @IBOutlet weak var hosAddress: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
     
     @IBAction func btnAddToFavorite(_ sender: UIButton) {
 //        let little_data_center:UserDefaults
@@ -26,8 +25,39 @@ class HosDetailViewController: UIViewController {
         super.viewDidLoad()
         self.hosTelephone.text = strtel
         self.hosAddress.text = straddr
+        getDestination()
     }
     
+    func getDestination()
+    {
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(hosAddress.text!) { (placemarks, error) in
+            if let err = error
+            {
+                print("轉碼錯誤\(err)")
+            }
+            else
+            {
+                let placemarks = placemarks
+                let location = placemarks?.first?.location as! CLLocation
+                //print(location.coordinate.latitude, location.coordinate.longitude)
+                self.latitude = location.coordinate.latitude
+                self.longitude = location.coordinate.longitude
+                print("latitude:\(self.latitude!),longitude:\(self.longitude!)")
+                
+                
+                //annomation.coordinate = CLLocationCoordinate2DMake(24.916062, 121.210480)
+                self.annomation.coordinate = CLLocationCoordinate2DMake(self.latitude, self.longitude)
+                self.annomation.title = self.hosName.text
+                //self.annomation.subtitle = self.hosAddress
+                self.mapView.addAnnotation(self.annomation)
+                
+                let region = MKCoordinateRegion(center: self.annomation.coordinate, latitudinalMeters: 100, longitudinalMeters: 100)
+                
+                self.mapView.setRegion(region, animated: true)
+            }
+        }
+    }
 
 
 }
