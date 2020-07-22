824976f4d0d8fdca843dd7f35e2ceb7293c55547
anna328537 <anna328537@gmail.com>
2020-07-22 10:50:26 +0800

    anna

--- patHosMap/AddAnimal.swift
+++ patHosMap/AddAnimal.swift
@@ -65,6 +65,7 @@ class AddAnimal: UIViewController,UIImagePickerControllerDelegate,UINavigationCo
         notificationCenter.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         
         notificationCenter.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        print("anna")
         // Do any additional setup after loading the view.
     }
     
