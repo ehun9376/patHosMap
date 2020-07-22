5a2d107a96bb71eb1fa43b66b77cb7b3635fdf2c
anna328537 <anna328537@gmail.com>
2020-07-22 10:13:37 +0800

    Addanimal

--- patHosMap/AddAnimal.swift
+++ patHosMap/AddAnimal.swift
@@ -14,9 +14,11 @@ class AddAnimal: UIViewController,UIImagePickerControllerDelegate,UINavigationCo
     
     @IBOutlet weak var txtName: UITextField!
     @IBOutlet weak var txtBirthday: UITextField!
-    
-    
-    
+    @IBOutlet weak var imgPicture: UIImageView!
+    @IBOutlet weak var btnCat: UIButton!
+    @IBOutlet weak var btnDog: UIButton!
+    @IBOutlet weak var btnDecide: UIButton!
+    @IBOutlet weak var btnEdit: UIButton!
     
     @objc func keyboardWillHide(){
         print("鍵盤收合！")
@@ -78,9 +80,28 @@ class AddAnimal: UIViewController,UIImagePickerControllerDelegate,UINavigationCo
         let imagePicker = UIImagePickerController()
         imagePicker.sourceType = .camera
         imagePicker.delegate = self
+        self.show(imagePicker, sender: nil)
     }
     
     //相簿
+    
+    @IBAction func btnPhotoAlbum(_ sender: UIButton) {
+        if !UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+               print("此裝置沒有相簿")
+               return
+           }
+           //初始化影像挑選控制器
+           let imagePicker = UIImagePickerController()
+           //設定影像挑選控制器為相機
+           imagePicker.sourceType = .photoLibrary
+           //允許編輯相片
+           imagePicker.allowsEditing = true
+           
+           //設定相機相關的代理事件
+           imagePicker.delegate = self
+           //開啟相簿
+           self.show(imagePicker, sender: nil)
+    }
     //Todo
     /*
     // MARK: - Navigation
@@ -94,11 +115,18 @@ class AddAnimal: UIViewController,UIImagePickerControllerDelegate,UINavigationCo
     //MARK - UIImagePickerControllerDelegate
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         print("影像資訊:\(info)")
+        if let image = info[.originalImage] as? UIImage{
+            //將拍照結果顯示在拍照位置
+            imgPicture.image = image
+            //由picker退掉相機畫面
+            picker.dismiss(animated: true, completion: nil)
+        }
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         //編輯用鍵盤使用結束後收起
         self.view.endEditing(true)
     }
     
+
 
 }
