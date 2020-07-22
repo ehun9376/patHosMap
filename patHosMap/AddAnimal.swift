fb129a2b7f6ef3a87558a9d6c1e22ccccb251b4b
ehun9376 <joh1234567892001@gmail.com>
2020-07-22 18:53:20 +0800

    555

--- patHosMap/AddAnimal.swift
+++ patHosMap/AddAnimal.swift
@@ -22,7 +22,7 @@ class AddAnimal: UIViewController,UIImagePickerControllerDelegate,UINavigationCo
     @IBOutlet weak var btnEdit: UIButton!
     
     var pkvBirthday:UIPickerView!
-    let arrBirthday = [["189"],["1","2","3"],["10","11","11","11","11"]]
+    let arrBirthday = [["189"],["10","11","11","11","11"]]
 
  
     override func viewDidLoad() {
